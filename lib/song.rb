5530e61f15a1f0cb807d7bf601e472da7b6f2c1b
isabellademetz <isabella21136@gmail.com>
2018-07-13 18:08:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,23 @@
 class Song
-nienty_nine_problmes = Song.new( "99 Problems", "Jay-z", "rap")
-@@ninety_nine_problems.name
-ninety_nine_problems.artist
-ninety_nine_problems.genre
+ninety_nine_problems_nine_problems = Song.new( "99 Problems", "Jay-z", "rap")
+@@ninety_nine_problems.artist
+@@ninety_nine_problems.genre
 @@count = 0
 def initialize
   @@count += 1
 end
+
+Song.count
+return count
+end
+
+Song.genres
+  @@genres = []
+end
+
+Song.artists
+  @@artists =[]
+end
+
+Song.genre_count
+  
