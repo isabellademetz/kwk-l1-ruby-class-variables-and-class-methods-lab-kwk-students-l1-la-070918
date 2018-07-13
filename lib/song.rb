f38cc05c40c04263f380937efb139e95864e0e0c
isabellademetz <isabella21136@gmail.com>
2018-07-13 18:02:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,9 @@
+class Song
 nienty_nine_problmes = Song.new( "99 Problems", "Jay-z", "rap")
 ninety_nine_problems.name
 ninety_nine_problems.artist
 ninety_nine_problems.genre
+@@count = 0
+def initialize
+  @@count += 1
+end
