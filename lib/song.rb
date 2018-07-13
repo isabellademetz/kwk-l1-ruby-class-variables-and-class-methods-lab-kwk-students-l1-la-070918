5fbd6764217bb9026866b9d7879dde65ab6b3f34
isabellademetz <isabella21136@gmail.com>
2018-07-13 18:22:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,5 @@
 class Song
-ninety_nine_problems_nine_problems = Song.new("99 Problems", "Jay-z", "rap")
-attr_accessor :name
-attr_accessor :artist
-attr_accessor :genre
+
 @@count == 0
 def initialize
   @@count += 1
